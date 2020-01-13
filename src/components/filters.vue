2a24f2044f47fdaf03746c4af1a13f8619071858
fa <sdfsdfsdf>
2020-01-13 23:24:41 +0100

    first commit

---
+++ src/components/filters.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "filters"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/searcher.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "searcher"
+    }
+</script>
+
+<style scoped>
+
+</style>
